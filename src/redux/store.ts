6d55a857aaa6f8fd9b3c4ab7d57e4bcf99a8f2e0
Vaibhav Kaul <vaibhav@frontrow.co.in>
2023-01-28 23:35:33 +0530

    Added seperate branch for staging deployment

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+
 const rootReducer = combineReducers({});
 
 export const store = createStore(rootReducer, composeWithDevTools());
